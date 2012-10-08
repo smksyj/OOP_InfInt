a16e3455500c8e62f072b7c4954f3151f9b19e10
Ryan Lee <rlee@cyrusian.com>
2012-10-08 22:18:45 +0900

    / ���

--- InfiniteInt/InfInt.cpp
+++ InfiniteInt/InfInt.cpp
@@ -90,32 +90,7 @@ bool operator<(const InfInt& self, const InfInt& other) {
 }
 
 InfInt operator+(const InfInt& self, const InfInt& other) {
-<<<<<<< HEAD
 	self.digits;
-=======
-	int carry = 0;
-	InfInt ret;
-	ret.digits.clear();
-
-	for ( int i = 0; i < self.digits.size() || i < other.digits.size() || carry == 1; i++ ) {
-		int result = 0;
-		if ( i < other.digits.size() ) {
-			result += (other.digits.at(i) - ASCII_POSITION);
-		}
-		if ( i < self.digits.size() ) {
-			result += (self.digits.at(i) - ASCII_POSITION);
-		}
-		result += carry;
-
-		if ( result > 10 ) {
-			ret.digits += result % 10 + ASCII_POSITION;
-			carry = 1;
-		} else {
-			ret.digits += result + ASCII_POSITION;
-			carry = 0;
-		}
-	}
->>>>>>> cd8e754c15ea00bb4717d43b73de95a71120ec71
 
 	return InfInt();
 }
